83502c936cc498e6b0af11df9371b67de0312d31
Felipe Leal <felipewanderson2020@gmail.com>
2020-10-21 19:08:48 -0300

    :sparkles: connection server

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
+import { REACT_APP_URL_SERVER } from '../.env.json';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_URL_SERVER,
+  baseURL: REACT_APP_URL_SERVER,
 });
 
 export default api;
