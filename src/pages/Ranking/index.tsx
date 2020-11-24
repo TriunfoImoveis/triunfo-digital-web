07ac3eba0bc6dd6b7e2a14f80a4ad2ecd7622d27
Felipe Leal <felipewanderson2020@gmail.com>
2020-11-24 16:30:36 -0300

    :sparkles: Ranking
    Finzalização do ranking, adição dos filtros de mes e ano

--- src/pages/Ranking/index.tsx
+++ src/pages/Ranking/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import Loader from 'react-loader-spinner';
 import { useAuth } from '../../context/AuthContext';
 import Header from '../../components/Header';
 
@@ -21,6 +22,7 @@ import {
   Name,
   VGV,
   ButtonGroup,
+  LoadingContainer,
 } from './styles';
 import { formatPrice } from '../../utils/format';
 import api from '../../services/api';
@@ -34,30 +36,98 @@ interface IRealtorData {
 
 const Ranking: React.FC = () => {
   const [realtors, setRealtors] = useState<IRealtorData[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [selected, setSelected] = useState(false);
   const [token] = useState(() => localStorage.getItem('@TriunfoDigital:token'));
   const { userAuth } = useAuth();
 
   useEffect(() => {
     const loadRanking = async () => {
-      const response = await api.get('/ranking', {
-        headers: {
-          authorization: `Bearer ${token}`,
-        },
-        params: {
-          city: userAuth.subsidiary.city,
-        },
-      });
-      const ranking = response.data;
-      const rankingFormatted = ranking.map(r => ({
-        id: r.id,
-        avatar_url: r.avatar_url,
-        name: r.name,
-        vgv: formatPrice(r.vgv),
-      }));
-      setRealtors(rankingFormatted);
+      setLoading(true);
+      try {
+        const response = await api.get('/ranking', {
+          headers: {
+            authorization: `Bearer ${token}`,
+          },
+          params: {
+            city: userAuth.subsidiary.city,
+          },
+        });
+        const ranking = response.data;
+        const rankingFormatted = ranking.map(r => ({
+          id: r.id,
+          avatar_url: r.avatar_url,
+          name: r.name,
+          vgv: formatPrice(r.vgv),
+        }));
+        setRealtors(rankingFormatted);
+        setLoading(false);
+      } catch (error) {
+        setLoading(false);
+      }
     };
     loadRanking();
   }, [token, userAuth.subsidiary.city]);
+
+  const handleSwichVGVToYear = async (filter: string) => {
+    const monthSystem = new Date().getMonth().toLocaleString();
+    setSelected(!selected);
+    switch (filter) {
+      case 'month': {
+        setLoading(true);
+        try {
+          const response = await api.get('/ranking', {
+            headers: {
+              authorization: `Bearer ${token}`,
+            },
+            params: {
+              city: userAuth.subsidiary.city,
+              month: monthSystem,
+            },
+          });
+          const ranking = response.data;
+          const rankingFormatted = ranking.map(r => ({
+            id: r.id,
+            avatar_url: r.avatar_url,
+            name: r.name,
+            vgv: formatPrice(r.vgv),
+          }));
+          setRealtors(rankingFormatted);
+          setLoading(false);
+        } catch (error) {
+          setLoading(false);
+        }
+        break;
+      }
+      case 'year': {
+        setLoading(true);
+        try {
+          const response = await api.get('/ranking', {
+            headers: {
+              authorization: `Bearer ${token}`,
+            },
+            params: {
+              city: userAuth.subsidiary.city,
+            },
+          });
+          const ranking = response.data;
+          const rankingFormatted = ranking.map(r => ({
+            id: r.id,
+            avatar_url: r.avatar_url,
+            name: r.name,
+            vgv: formatPrice(r.vgv),
+          }));
+          setRealtors(rankingFormatted);
+          setLoading(false);
+        } catch (error) {
+          setLoading(false);
+        }
+        break;
+      }
+      default:
+        break;
+    }
+  };
 
   return (
     <Container>
@@ -73,55 +143,74 @@ const Ranking: React.FC = () => {
               <span className="nameTitle">Nome</span>
               <span className="vgvTitle">VGV</span>
               <ButtonGroup>
-                <button type="button">VGV</button>
-                <button type="button" className="selected">
+                <button
+                  type="button"
+                  className={selected ? 'selected' : undefined}
+                  onClick={() => handleSwichVGVToYear('month')}
+                >
                   Mês
+                </button>
+                <button
+                  type="button"
+                  className={!selected ? 'selected' : undefined}
+                  onClick={() => handleSwichVGVToYear('year')}
+                >
+                  Ano
                 </button>
               </ButtonGroup>
             </LabelItems>
           </LabelContainer>
-          <PodiumContainer>
-            {realtors.map((realtor, i) => {
-              return i <= 4 ? (
-                <PositionItem key={realtor.id}>
-                  <Position>{`${i + 1}°`}</Position>
-                  <Realtor>
-                    <img
-                      src={
-                        realtor.avatar_url || 'https://imgur.com/I80W1Q0.png'
-                      }
-                      alt={realtor.name}
-                    />
-                    <Name>
-                      <span>{realtor.name}</span>
-                    </Name>
-                    <VGV position={String(i + 1)}>
-                      <strong>{realtor.vgv}</strong>
-                    </VGV>
-                  </Realtor>
-                </PositionItem>
-              ) : null;
-            })}
-          </PodiumContainer>
-          <Separator />
-          <RealtorContainer>
-            {realtors.map((realtor, i) => {
-              return i >= 5 ? (
-                <PositionItem key={realtor.id}>
-                  <Position>{`${i}°`}</Position>
-                  <Realtor>
-                    <img src={realtor.avatar_url} alt={realtor.name} />
-                    <Name>
-                      <span>{realtor.name}</span>
-                    </Name>
-                    <VGV position={String(i)}>
-                      <strong>{realtor.vgv}</strong>
-                    </VGV>
-                  </Realtor>
-                </PositionItem>
-              ) : null;
-            })}
-          </RealtorContainer>
+          {loading ? (
+            <LoadingContainer>
+              <Loader type="Bars" color="#c32925" height={100} width={100} />
+            </LoadingContainer>
+          ) : (
+            <>
+              <PodiumContainer>
+                {realtors.map((realtor, i) => {
+                  return i <= 4 ? (
+                    <PositionItem key={realtor.id}>
+                      <Position>{`${i + 1}°`}</Position>
+                      <Realtor>
+                        <img
+                          src={
+                            realtor.avatar_url ||
+                            'https://imgur.com/I80W1Q0.png'
+                          }
+                          alt={realtor.name}
+                        />
+                        <Name>
+                          <span>{realtor.name}</span>
+                        </Name>
+                        <VGV position={String(i + 1)}>
+                          <strong>{realtor.vgv}</strong>
+                        </VGV>
+                      </Realtor>
+                    </PositionItem>
+                  ) : null;
+                })}
+              </PodiumContainer>
+              <Separator />
+              <RealtorContainer>
+                {realtors.map((realtor, i) => {
+                  return i >= 5 ? (
+                    <PositionItem key={realtor.id}>
+                      <Position>{`${i}°`}</Position>
+                      <Realtor>
+                        <img src={realtor.avatar_url} alt={realtor.name} />
+                        <Name>
+                          <span>{realtor.name}</span>
+                        </Name>
+                        <VGV position={String(i)}>
+                          <strong>{realtor.vgv}</strong>
+                        </VGV>
+                      </Realtor>
+                    </PositionItem>
+                  ) : null;
+                })}
+              </RealtorContainer>
+            </>
+          )}
         </RankingContainer>
       </Content>
     </Container>
