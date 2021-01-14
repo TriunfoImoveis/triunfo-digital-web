973de5939dbccebcc1d235548da841daa344be23
Felipe Leal <felipewanderson2020@gmail.com>
2021-01-14 17:12:46 -0300

    token bug

--- src/context/FormContext.tsx
+++ src/context/FormContext.tsx
@@ -1,10 +1,4 @@
-import React, {
-  createContext,
-  useContext,
-  useState,
-  useCallback,
-  useEffect,
-} from 'react';
+import React, { createContext, useContext, useState, useCallback } from 'react';
 import { toast } from 'react-toastify';
 import api from '../services/api';
 
