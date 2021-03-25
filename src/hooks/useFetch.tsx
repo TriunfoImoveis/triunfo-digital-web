7b053710077103514d3b9410de5b69177d4ff092
Felipe Leal <felipewanderson2020@gmail.com>
2021-03-25 16:45:35 -0300

    :sparkles: revalidate cache

--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -2,10 +2,18 @@ import useSWR from 'swr';
 import api from '../services/api';
 
 export function useFetch<Data = any, Error = any>(url: string) {
-  const { data, error } = useSWR<Data, Error>(url, async url => {
-    const response = await api.get(url);
-    return response.data;
-  });
+  const { data, error } = useSWR<Data, Error>(
+    url,
+    async url => {
+      const response = await api.get(url);
+      return response.data;
+    },
+    {
+      revalidateOnFocus: true,
+      refreshInterval: 1000,
+      revalidateOnReconnect: true,
+    },
+  );
 
   return { data, error };
 }
